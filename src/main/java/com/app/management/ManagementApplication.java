72b58bdb99db2316635e18f18d83e02ee5a16fa2
aleeeeeexcp <acampar11@gmail.com>
2026-02-04 10:37:28 +0100

    Changed application.properties to protect my db credentials and jwt key

--- src/main/java/com/app/management/ManagementApplication.java
+++ src/main/java/com/app/management/ManagementApplication.java
@@ -5,10 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ManagementApplication {
 
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			if (System.getenv(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
 		SpringApplication.run(ManagementApplication.class, args);
 	}
 
